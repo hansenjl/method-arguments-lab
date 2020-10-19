86504f0844bb9795b6aa0c4931d474f842388ee6
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-19 12:49:00 -0700

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,1 +1,8 @@
-hi there
+require 'pry'
+def introduction(whateverwewant) # expecting 1 arguments
+    puts "Hi, my name is #{whateverwewant}."
+end 
+
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
